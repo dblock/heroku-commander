5c3ec69019451a8ed871107811e51d5760290a62
dblock <dblock@dblock.org>
2013-02-01 00:45:20 -0500

    Give up on Ruby 1.8.7, PTY.spawn is not reliable in reading STDOUT.

--- spec/heroku/executor_spec.rb
+++ spec/heroku/executor_spec.rb
@@ -1,28 +1,23 @@
 require 'spec_helper'
 
 describe Heroku::Executor do
-  let :logger do
-    logger = Logger.new($stdout)
-    logger.level = Logger::DEBUG
-    logger
-  end
   context "missing argument" do
     subject { lambda { Heroku::Executor.run } }
     it { should raise_error ArgumentError }
   end
   context "command does not exist" do
-    subject { lambda { Heroku::Executor.run "executor_spec.rb", { :logger => logger } } }
+    subject { lambda { Heroku::Executor.run "executor_spec.rb" } }
     it { should raise_error Heroku::Commander::Errors::CommandError, /The command `executor_spec.rb` failed with exit status \d+./ }
   end
   context "command exists" do
-    subject { lambda { Heroku::Executor.run "ls -1", { :logger => logger } } }
+    subject { lambda { Heroku::Executor.run "ls -1" } }
     it { should_not raise_error }
     its(:call) { should include "Gemfile" }
   end
   context "line-by-line" do
     it "yields" do
       lines = []
-      Heroku::Executor.run "ls -1", { :logger => logger } do |line|
+      Heroku::Executor.run "ls -1" do |line|
         lines << line
       end
       lines.should include "Gemfile"
